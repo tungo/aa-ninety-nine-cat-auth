e5c76ff10c5048bf380fd55266fb5ed86d8a440b
Tu Ngo <tungo8810@gmail.com>
2017-04-19 14:40:30 -0700

    Add User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,33 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token
 
+  #all these methods are on server's side
   def ensure_session_token
 
   end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+  end
+
+  def password=(password)
+    @password = password  # ? why store password in instance var?
+    password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by(username: username) # NOT from params, username: from users table column 'username'
+    return user if user && user.is_password?(password)
+    nil
+  end
+
 end
