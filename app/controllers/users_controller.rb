a0a4e8c9745853edf259ed26fa0154687a126a31
Tu Ngo <tungo8810@gmail.com>
2017-04-22 10:27:21 -0700

    Add UsersController methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
-  
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to root_url
+    else
+      flash.now[:errors] = user.errors.full_message
+      render :new
+    end
+  end
 end
