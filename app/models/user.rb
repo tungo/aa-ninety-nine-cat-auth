c2827a40309bfd26eb2cbb17a1dd02d9f36f32a3
Tu Ngo <tungo8810@gmail.com>
2017-04-22 10:23:41 -0700

    Add UpdateUsers migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ActiveRecord::Base
     SecureRandom::urlsafe_base64(16)
   end
 
+  def self.find_by_credentials(username, password)
+    user = find_by(username: username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
   def ensure_session_token
     self.session_token ||= self.class.generate_session_token
   end
@@ -29,13 +35,7 @@ class User < ActiveRecord::Base
   end
 
   def is_password?(password)
-    BCrypt::Password.new(password_digest).is_password?(password)
-  end
-
-  def self.find_by_credentials(username, password)
-    user = self.find_by(username: username)
-    return user if user && user.is_password?(password)
-    nil
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
 end
